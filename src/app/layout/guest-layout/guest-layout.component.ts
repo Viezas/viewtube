ad26775cbb34764dfc2188a3fcfdd2ea9e344aef
Viezas <dxviezas@gmail.com>
2023-12-09 16:59:54 +0100

    Component for app logo

--- src/app/layout/guest-layout/guest-layout.component.ts
+++ src/app/layout/guest-layout/guest-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { LogoComponent } from '../../components/logo/logo.component';
 
 @Component({
   selector: 'app-guest-layout',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, LogoComponent],
   templateUrl: './guest-layout.component.html',
   styleUrl: './guest-layout.component.css'
 })
